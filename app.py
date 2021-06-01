c9bb599728b572f0680ce29abe7bae1c0eec09d2
Mat Clark <mclark116@gmail.com>
2021-06-01 15:11:28 -0500

    Editing Article Entry
    Formatting the full article page. Figuring out a way to format text in a <textarea>.

--- app.py
+++ app.py
@@ -15,6 +15,26 @@ def reviews():
 def youtube():
     return render_template('flexgridblock.html')
 
+@app.route('/fullarticle/<id>')
+def full(id):
+    article = Articles.query.get(id)
+    return render_template('fullarticle.html', article=article)
+
+
+@app.route('/edit/<id>', methods=['GET', 'POST'])
+def edit_article(id):
+    article = Articles.query.get(id)
+    if request.form:
+        article.name = request.form['name']
+        article.title = request.form['title']
+        article.sub_title = request.form['article_subtitle']
+        article.body = request.form['description']
+        article.url = request.form['url']
+        db.session.commit()
+        return redirect(url_for('index'))
+    return render_template('edit.html', article=article)
+    
+
 @app.route('/form', methods=['GET', 'POST'])
 def form():
     if request.form:
@@ -24,6 +44,8 @@ def form():
         db.session.commit()
         return redirect(url_for('index'))
     return render_template('form.html')
+
+
 
 
 if __name__ == '__main__':
